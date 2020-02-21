f8a1af8f156a39b4ee9977c3f1be20849c6168d2
NauhWuun <geteasycn@126.com>
2020-02-21 15:15:47 +0800

    fix codes

--- src/java/Columnar/ColumnTree.java
+++ src/java/Columnar/ColumnTree.java
@@ -1,13 +1,11 @@
 package java.Columnar;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.IdentityHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ColumnTree 
 {
@@ -16,28 +14,32 @@ public class ColumnTree
     private Node rootNode;
     private int currentIndex = 0;
 
-    private static Map<Object, Object> kvMaps = new IdentityHashMap<>();
-    private static final Map<Object, KV> kvNodes = new ConcurrentHashMap<>();
+    private static Map<Object, KV> kvMaps = new HashMap<>();
+    private static final List<Node> kvNodes = new ArrayList<>();
 
     public ColumnTree() { 
         /**
          * Build Default Root in Column Trees
          */
         KV rootKv = new KV(ROOT, "left", "right");
-        rootNode = new Node(ROOT, rootKv);
+        rootNode = new Node(rootKv);
 
-        kvMaps.put("left", "right");
-        kvNodes.put(rootKv, rootKv);
+        kvNodes.add(rootNode);
     }
 
     /**
      * addtion new cloumn data(s)
      */
-    public void add(Object parent, Object key, Object value) {
-        kvMaps.put(key, value);
+    public void put(Object parent, Object key, Object value) {
+        boolean hasKey = kvMaps.keySet().stream().allMatch(action -> action.equals(parent));
+
         KV newKV = new KV(parent, key, value);
 
-        kvNodes.put(parent, newKV);
+        if (hasKey) {    
+            kvMaps.put(parent, newKV);
+        } else {
+            kvNodes.add(new Node(newKV));
+        }
 
         currentIndex++;
     }
@@ -45,9 +47,8 @@ public class ColumnTree
     /**
      * delete cloumn data, params(Column Name/Mapping Key)
      */
-    public void delete(Object parent, Object key) {
-        kvMaps.remove(key);
-        kvNodes.remove(parent);
+    public void delete(Object parent) {
+        kvMaps.remove(parent);
 
         --currentIndex;
     }
@@ -58,7 +59,7 @@ public class ColumnTree
     public List<KV> getParentListKV(Object parent) {
         List<KV> newList = new ArrayList<>();
 
-        Set<Entry<Object, KV>> entry = kvNodes.entrySet();
+        Set<Entry<Object, KV>> entry = kvMaps.entrySet();
         entry.parallelStream().forEach(action -> {
             if (action.getKey().equals(parent))
                 newList.add(action.getValue());
@@ -88,7 +89,7 @@ public class ColumnTree
     }
 
     public Object getParentWithKey(Object key) {
-        return kvNodes.values().parallelStream().filter(
+        return kvMaps.values().parallelStream().filter(
             predicate -> predicate.getLeft().equals(key))
                 .findFirst().map(
                     mapper -> mapper.getParent()
@@ -97,7 +98,7 @@ public class ColumnTree
     }
 
     public Object getParentWithValue(Object value) {
-        return kvNodes.values().parallelStream().filter(
+        return kvMaps.values().parallelStream().filter(
             predicate -> predicate.getRight().equals(value))
                 .findFirst().map(
                     mapper -> mapper.getParent()
@@ -106,11 +107,11 @@ public class ColumnTree
     }
 
     public List<Object> getParent() {
-        return new ArrayList<>(kvNodes.keySet());
+        return new ArrayList<>(kvMaps.keySet());
     }
 
     public boolean contains(Object parent) {
-        return kvNodes.containsKey(parent);
+        return kvMaps.containsKey(parent);
     }
 
     public Object previous() {
@@ -155,21 +156,16 @@ public class ColumnTree
      * |  key            value    |
      * |==========================|           
      */
-    private static class Node
+    class Node
     {
-        Object parent;
+        KV kv;
 
-        public Node(Object parent, KV kv) {
-            this.parent = parent;
-            kvNodes.put(parent, kv);
+        private Node(final KV kv) {
+            this.kv = kv;
         }
 
-        public final Object getParent() {
-            return parent;
-        }
-
-        public final KV getParentKV(final Object parent) {
-            return kvNodes.get(parent);
+        public final KV getKV() {
+            return kv;
         }
     }
 
@@ -188,23 +184,16 @@ public class ColumnTree
      *  [index] <=>  [index]
      * =========================
      */
-    public class KV implements Serializable
+    class KV
     {
-        /**
-         *
-         */
-        private static final long serialVersionUID = -2956614816449059957L;
-
+        Object parent;
         Object left;
         Object right;
-        Object parent;
 
         public KV(final Object parent, final Object left, final Object right) {
+            this.parent = parent;
             this.left = left;
             this.right = right;
-            this.parent = parent;
-
-            kvMaps.put(left, right);
         }
 
         public final Object getLeft() {
@@ -215,7 +204,7 @@ public class ColumnTree
             return right;
         }
 
-        public final Object getParent() {
+        public Object getParent() {
             return parent;
         }
     }
